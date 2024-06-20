fd8d97e3e9e3e6e8acf25f21fe223b628374eb50
StanDobrev11 <dobrev81@gmail.com>
2024-06-20 21:33:33 +0000

    update

---
+++ 01_js_syntax_fundamentals/02-student-information.js
@@ -0,0 +1,7 @@
+// 'John', 15, 5.54678
+
+function solve(name, age, grade) {
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`)
+}
+
+// solve('John', 15, 5.54678)
---
+++ 01_js_syntax_fundamentals/03-excellent-grade.js
@@ -0,0 +1,10 @@
+function solve(value) {
+    if (value >= 5.5) {
+        console.log('Excellent')
+    } else {
+        console.log('Not Excellent')
+    }
+}
+
+solve(5)
+solve(5.6)
