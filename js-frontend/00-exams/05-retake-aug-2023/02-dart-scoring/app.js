3d0536a8d4b7316c70d78d256a209f1aa2fee06a
StanDobrev11 <dobrev81@gmail.com>
2024-07-19 00:57:53 +0000

    update

--- js-frontend/00-exams/05-retake-aug-2023/02-dart-scoring/app.js
+++ js-frontend/00-exams/05-retake-aug-2023/02-dart-scoring/app.js
@@ -1,6 +1,149 @@
 window.addEventListener("load", solve);
 
 function solve() {
-    //TODO
-  }
+
+    // change here
+    const addButtonId = 'add-btn'
+    const editButtonClass = 'btn edit'
+    const editButtonText = 'edit'
+    const doneButtonclass = 'btn ok'
+    const doneButtonText = 'ok'
+
+    const firstInputId = 'player'
+    const secondInputId = 'score'
+    const thirdInputId = 'round'
+    const ulFromInputId = 'sure-list'
+    const ulFinalId = 'scoreboard-list'
+    const liElementClassName = 'dart-item'
+
+
+    const addButton = document.getElementById(addButtonId)
+    const deleteButton = document.querySelector(`.btn.clear`)
+    const firstInput = document.getElementById(firstInputId)
+    const secondInput = document.getElementById(secondInputId)
+    const thirdInput = document.getElementById(thirdInputId)
+
+    const ulFromInput = document.getElementById(ulFromInputId)
+    const ulFinalElement = document.getElementById(ulFinalId)
+
+    const inputMapper = {}
+    inputMapper[firstInputId] = firstInput      
+    inputMapper[secondInputId] = secondInput
+    inputMapper[thirdInputId] = thirdInput
+    
+    deleteButton.addEventListener('click', () => {
+        location.reload()
+    })
+
+    addButton.addEventListener('click', () => {
+        if (checkValidInput()) {
+            const liElement = createLiRecordElement()
+            ulFromInput.appendChild(liElement)
+            clearInputFields()
+            addButton.setAttribute('disabled', true)
+        }
+    })
+
+    function createDetailsArticle() {
+        const articleElement = document.createElement('article')
+
+        for (let i = 0; i < Object.keys(inputMapper).length; i++) {
+            inputId = Object.keys(inputMapper)[i]
+            const pElement = document.createElement('p')
+            const name = capsFirstLetter(inputId)
+            if (i === 0) {
+                pElement.textContent = inputMapper[inputId].value
+            } else {
+                pElement.textContent = `${name}: ${inputMapper[inputId].value}`
+            }
+            articleElement.appendChild(pElement)
+        }
+        // Object.keys(inputMapper)
+        //     .forEach(inputId => {
+        //         const pElement = document.createElement('p')
+        //         const name = capsFirstLetter(inputId)
+        //         pElement.textContent = `${name}:${inputMapper[inputId].value}`
+        //         articleElement.appendChild(pElement)
+        //     })
+
+        return articleElement
+    }
+
+    function populateInputFields(inputValues) {
+        inputsArray().map((input, i) => input.value = inputValues[i])
+    }
+
+    function createButtons() {
+        
+        const divFragment = document.createDocumentFragment()
+
+        const editButton = document.createElement('button')
+        editButton.textContent = editButtonText
+        editButton.className = editButtonClass
+        editButton.addEventListener('click', (e) => {
+            const liElement = e.target.parentElement
+            const articleElement = liElement.querySelector('article')
+            const inputValues = []
+            for (let i = 0; i < Array.from(articleElement.children).length; i++) {
+                if (i === 0) {
+                    inputValues.push(Array.from(articleElement.children)[i].textContent)
+                } else {
+                    inputValues.push(Array.from(articleElement.children)[i].textContent.split(': ')[1])
+                }
+            }
+            populateInputFields(inputValues)
+            addButton.removeAttribute('disabled')
+            liElement.remove()
+        })
+
+
+        const doneButton = document.createElement('button')
+        doneButton.textContent = doneButtonText
+        doneButton.className = doneButtonclass
+        doneButton.addEventListener('click', (e) => {
+            const liElement = e.target.parentElement
+            ulFinalElement.appendChild(liElement)
+            addButton.removeAttribute('disabled')
+            editButton.remove()
+            doneButton.remove()
+        })
+
+        divFragment.appendChild(editButton)
+        divFragment.appendChild(doneButton)
+
+        return divFragment
+    }
+
+
+    function createLiRecordElement() {
+
+        const liElement = document.createElement('li')
+        liElement.className = liElementClassName
+
+        const articleElement = createDetailsArticle()
+        const divButtonsElement = createButtons()
+
+        liElement.appendChild(articleElement)
+        liElement.appendChild(divButtonsElement)
+
+        return liElement
+    }
+
+    function capsFirstLetter(text) {
+        return text.split('')[0].toUpperCase() + text.substring(1)
+    }
+
+    function clearInputFields() {
+        inputsArray().map(input => input.value = '')
+    }
+
+    function inputsArray() {
+        return Object.values(inputMapper)
+    }
+
+    function checkValidInput() {
+        return inputsArray().every(input => input.value !== '')
+    }
+
+    }
   
--- js-frontend/00-exams/05-retake-aug-2023/03-weather-tracker/app.js
+++ js-frontend/00-exams/05-retake-aug-2023/03-weather-tracker/app.js
@@ -1,1 +1,188 @@
-//TODO...
+const baseUrl = 'http://localhost:3030/jsonstore/tasks'
+
+// change here
+const loadButtonId = 'load-history'
+const addButtonId = 'add-weather'
+const editButtonId = 'edit-weather'
+const firstInputId = 'location'
+const secondInputId = 'temperature'
+const thirdInputId = 'date'
+const itemListElementId = 'list'
+
+
+// create elements
+const divElementClass = 'container'
+const divButtonsId = 'buttons-container'
+const changeButtonClass = 'change-btn'
+const changeButtonText = 'Change'
+const deleteButtonClass = 'delete-btn'
+const deleteButtonText = 'Delete'
+
+// not here
+const loadButton = document.getElementById(loadButtonId)
+const addButton = document.getElementById(addButtonId)
+const editButton = document.getElementById(editButtonId)
+
+const firstInput = document.getElementById(firstInputId)
+const secondInput = document.getElementById(secondInputId)
+const thirdInput = document.getElementById(thirdInputId)
+const itemListElement = document.getElementById(itemListElementId)
+
+
+addButton.addEventListener('click', async () => {
+    try {
+        await fetch(baseUrl, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                location: firstInput.value,
+                temperature: secondInput.value,
+                date: thirdInput.value
+            })
+        });
+
+        getAllRequest();
+        clearInputFields();
+
+    } catch (error) {
+        console.error(error);
+    }
+});
+
+
+editButton.addEventListener('click', async () => {
+    
+    const id = editButton.value
+    try {
+        await fetch(`${baseUrl}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                location: firstInput.value,
+                temperature: secondInput.value,
+                date: thirdInput.value,
+                _id: id
+            })
+        })
+
+        getAllRequest()
+        clearInputFields()
+
+        addButton.removeAttribute('disabled')
+        editButton.value = ''
+        editButton.setAttribute('disabled', '')
+        
+    } catch (err) { console.log(err.message) }
+})
+
+function clearInputFields() {
+    firstInput.value = '', 
+    secondInput.value = '', 
+    thirdInput.value = ''
+}
+
+loadButton.addEventListener('click', getAllRequest)
+
+async function getAllRequest() {
+    itemListElement.innerHTML = ''
+    
+    try {
+        const promiseResponse = await fetch(baseUrl)
+        const allData = await promiseResponse.json()
+        
+        Object.values(allData)
+            .forEach(item => {
+                const divElement = createDivItemElement(item)
+                itemListElement.appendChild(divElement)
+            })
+    } catch(err) {console.log(err.message)}
+}
+
+function createInnerHElements(item) {
+
+    const divFragment = document.createDocumentFragment()
+
+    for (let i = 0; i < Object.keys(item).length - 1; i++) {
+        if (i === 0) {
+            const h2Element = document.createElement('h2')
+            h2Element.textContent = item[Object.keys(item)[i]]
+            divFragment.appendChild(h2Element)
+        } else {
+            const h3Element = document.createElement('h3')
+            h3Element.textContent = item[Object.keys(item)[i]]
+            divFragment.appendChild(h3Element)
+        }
+    }
+
+    return divFragment
+
+}
+
+function populateInputFields(details) {
+    [firstInput.value, secondInput.value, thirdInput.value] = details
+}
+
+function createButtonsDiv() {
+    const divButtons = document.createElement('div')
+    divButtons.setAttribute('id', divButtonsId)
+
+    const changeButton = document.createElement('button')
+    changeButton.textContent = changeButtonText
+    changeButton.className = changeButtonClass
+    changeButton.addEventListener('click', (e) => {
+        const divElement = e.target.parentElement.parentElement
+        const id = divElement.getAttribute('id')
+
+        const details = []
+        details.push(divElement.querySelector('h2').textContent)
+        details.push(divElement.querySelector('h3:first-of-type').textContent)
+        details.push(divElement.querySelector('h3:last-of-type').textContent)
+        // const divContentElement = divElement.querySelector('.content')
+        // const details = Array.from(divContentElement.children).map(pEl => pEl.textContent)
+
+        populateInputFields(details)
+      
+        editButton.removeAttribute('disabled')
+        editButton.value = id
+        addButton.setAttribute('disabled', '')
+
+    })
+
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = deleteButtonText
+    deleteButton.className = deleteButtonClass
+
+    deleteButton.addEventListener('click', async (e) => {
+        const divElement = e.target.parentElement.parentElement
+        const id = divElement.getAttribute('id')
+
+        await fetch(`${baseUrl}/${id}`, {
+            method: 'DELETE'
+        })
+        divElement.remove()
+    })
+    
+    divButtons.appendChild(changeButton)
+    divButtons.appendChild(deleteButton)
+
+    return divButtons
+}
+
+function createDivItemElement(item) {
+    
+    const divElement = document.createElement('div')
+    divElement.className = divElementClass
+    divElement.setAttribute('id', item._id)
+
+    const divDetailsElement = createInnerHElements(item)
+    const divButtonsElement = createButtonsDiv()
+
+    divElement.appendChild(divDetailsElement)
+    divElement.appendChild(divButtonsElement)
+
+    return divElement
+}
